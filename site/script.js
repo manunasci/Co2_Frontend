b84079cacf194f1b5de836c35f747d387fe0e97f
viana4417 <viana-santos2005@hotmail.com>
2024-10-16 00:39:24 -0300

    altearações

--- site/script.js
+++ site/script.js
@@ -10,6 +10,12 @@ function fecharMenu() {
 
 function esconderGrafico() {
 
+    const co2Info = document.getElementById('co2-info');
+    const vendasInfo = document.getElementById('vendas-informacoes');
+    
+    co2Info.style.display = 'none';
+    vendasInfo.style.display = 'none'; 
+
     fecharMenu();
 
     esconderMiniaturas();
@@ -20,6 +26,12 @@ function esconderGrafico() {
 }
 
 function mostrarGrafico(tipoGrafico) {
+
+    const co2Info = document.getElementById('co2-info');
+    const vendasInfo = document.getElementById('vendas-informacoes');
+    
+    co2Info.style.display = 'none';
+    vendasInfo.style.display = 'none'; 
 
     esconderMiniaturas();
     document.getElementById('grafico').remove();
@@ -94,7 +106,7 @@ function esconderMiniaturas() {
 }
 
 
-var dadosGrafico1 = {labels: [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021],
+var dadosGrafico2 = {labels: [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021],
 datasets: [{
     label: 'Venda de Veículos Elétricos (EUA)',
     backgroundColor: 'rgba(135, 235, 34)',
@@ -102,7 +114,7 @@ datasets: [{
     borderWidth: 1,
     data: [5105.414, 39967.161, 129399.455, 269820.792, 409440.918, 516800.901, 736631.264, 964601.649, 1495602.809, 1793702.759, 2052303.113, 2876105.719] 
 }]};
-var dadosGrafico2 = {labels: [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021],
+var dadosGrafico1 = {labels: [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021],
 datasets: [{
     label: 'Emissão de CO2 (EUA)',
     backgroundColor: 'rgba(34, 135, 235)',
@@ -113,7 +125,8 @@ datasets: [{
 
 function mostrarMiniatura(id, dados) {
     var miniaturaContainer = document.getElementById(id);
-  
+    miniaturaContainer.innerHTML = ''; 
+    
     var canvas = document.createElement('canvas');
     canvas.width = 30;
     canvas.height = 30;
@@ -136,7 +149,10 @@ function mostrarMiniatura(id, dados) {
     miniaturas.forEach(function(miniatura) {
         miniatura.classList.remove('escondido');
     });
+
 }
+
+
 
 var graficos = {
     grafico1: document.getElementById('grafico1').getContext('2d'),
@@ -144,12 +160,19 @@ var graficos = {
 };
 
 function mostrarGraficos2() {
+
+    const co2Info = document.getElementById('co2-info');
+    const vendasInfo = document.getElementById('vendas-informacoes');
+    
+    co2Info.style.display = 'block';
+    vendasInfo.style.display = 'block'; 
+
     var graficoCanvas = document.getElementById('grafico');
     graficoCanvas.style.display = 'none';
 
     document.getElementById('miniatura1').innerHTML = '';
     document.getElementById('miniatura2').innerHTML = '';
-
+    
     mostrarMiniatura('miniatura1', dadosGrafico1);
     mostrarMiniatura('miniatura2', dadosGrafico2);
 }
